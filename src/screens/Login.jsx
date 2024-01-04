90ccb9e21848b14e2ea304e2f69a0b2090839b9b
Mike Leman <mikeeleman@gmail.com>
2024-01-04 13:01:33 +0100

    test login function

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,7 +1,24 @@
 import {View} from "react-native";
 import {Text,} from "react-native-paper";
 import {Input, DefaultView, InputText, ActionButton, Title} from "../constants/style/styled";
+import {useState} from "react";
 export  const Login = ({navigation}) => {
+    const [login, setLogin] = useState({
+        email: '',
+        password: ''
+    })
+    const testLogin = async () => {
+        const req = await fetch('https://matepair.mikeleman.fr/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: login
+        })
+        const res = await req.json()
+        console.log(res)
+        }
+
     return (
         <DefaultView style={{
             justifyContent: 'center',
@@ -14,15 +31,22 @@ export  const Login = ({navigation}) => {
                     underlineColor="transparent"
                     theme={{  colors: { text: '#EEEEEE'} }}
                     textColor={'#EEEEEE'}
-                    activeUnderlineColor={'#EEEEEE'}/>
+                    activeUnderlineColor={'#EEEEEE'}
+                    onChangeText={text => setLogin({...login, email: text})}
+                />
                 <InputText>Password</InputText>
                 <Input
                     underlineColor="transparent"
                     theme={{  colors: { text: '#EEEEEE'} }}
                     textColor={'#EEEEEE'}
-                    activeUnderlineColor={'#EEEEEE'}/>
+                    activeUnderlineColor={'#EEEEEE'}
+                    onChangeText={text => setLogin({...login, password: text})}
+                />
                 <Text style={{color: '#EEEEEE', marginTop:10, fontWeight: 'bold'}}>Mot de passe oublié ?</Text>
             </View>
+            <ActionButton mode="contained" onPress={()=> testLogin()}>
+                Login
+            </ActionButton>
             <ActionButton mode="contained" onPress={()=> navigation.navigate('Home')}>
                 Go to Home
             </ActionButton>
