f42dc458738fd6b11de6b7db792fed93b59e86e0
Mike Leman <mikeeleman@gmail.com>
2024-01-04 11:13:07 +0100

    navigation and add dependency

--- src/screens/Message.jsx
+++ src/screens/Message.jsx
@@ -1,9 +1,13 @@
 import {SafeAreaView, Text} from "react-native";
+import {Button} from "react-native-paper";
 
-export const Message = () => {
+export const Message = ({navigation}) => {
     return (
         <SafeAreaView>
             <Text>Message</Text>
+            <Button mode="contained" onPress={()=> navigation.navigate('Home')}>
+                Go to Home
+            </Button>
         </SafeAreaView>
     )
 }
