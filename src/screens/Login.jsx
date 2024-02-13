b41d63208d8163498650bbf8652251cb62a9d03c
Mike Leman <mikeeleman@gmail.com>
2024-02-13 16:51:59 +0100

    login fetch

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -3,68 +3,72 @@ import {IconButton, Text,} from "react-native-paper";
 import {Input, DefaultView, InputText, ActionButton, Title, ActionButtonText} from "../constants/style/styled";
 import {useState} from "react";
 import {HeaderButton, HeaderContainer, HeaderText} from "../constants/style/auth/styled";
-import { Ionicons } from '@expo/vector-icons';
+import {Ionicons} from '@expo/vector-icons';
 
-export  const Login = ({navigation}) => {
-    const userJson = {
-        "id": 1,
-        "email": "user@example.com",
-        "roles": ["ROLE_USER"],
-        "password": "hashed_password",
-        "username": "user123",
-        "details": "Some details about the user",
-        "favorite_games": ["Game1", "Game2"],
-        "play_schedule": ["Monday", "Wednesday"],
-        "send": [],
-        "receive": [],
-        "notifications": [],
-        "first": [],
-        "second": [],
-        "weekDays": [],
-        "timeDays": []
-    }
+export const Login = ({navigation}) => {
+    // const userJson = {
+    //     "id": 1,
+    //     "email": "user@example.com",
+    //     "roles": ["ROLE_USER"],
+    //     "password": "hashed_password",
+    //     "username": "user123",
+    //     "details": "Some details about the user",
+    //     "favorite_games": ["Game1", "Game2"],
+    //     "play_schedule": ["Monday", "Wednesday"],
+    //     "send": [],
+    //     "receive": [],
+    //     "notifications": [],
+    //     "first": [],
+    //     "second": [],
+    //     "weekDays": [],
+    //     "timeDays": []
+    // }
     const [login, setLogin] = useState({
         email: '',
         password: ''
     })
     const auth = async () => {
-        if (login.email === userJson.email && login.password === userJson.password) {
-           console.log('ok')
+        if (login.email && login.password) {
+            const req = await fetch('https://mikeleman.fr/auth', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(login)
+            })
+            const res = await req.json()
+            console.log(res)
         }
 
-        }
+    }
+
 
     return (
         <DefaultView style={{padding: 10}}>
-           <HeaderContainer style={{margin:10}}>
-               <HeaderButton>
-                   <Ionicons name="ios-caret-back"  size={20} color="#8457AA" onPress={()=>navigation.goBack()}/>
-               </HeaderButton>
-               <HeaderText>S'inscrire</HeaderText>
-              </HeaderContainer>
-            <View style={{marginBottom : 10, paddingLeft: 10, paddingRight: 10, flex : 1, justifyContent:'center'}}>
+            <HeaderContainer style={{margin: 10}}>
+                <HeaderButton>
+                    <Ionicons name="ios-caret-back" size={20} color="#8457AA" onPress={() => navigation.goBack()}/>
+                </HeaderButton>
+                <HeaderText>S'inscrire</HeaderText>
+            </HeaderContainer>
+            <View style={{marginBottom: 10, paddingLeft: 10, paddingRight: 10, flex: 1, justifyContent: 'center'}}>
                 <InputText>Email</InputText>
-                <Input
-                    underlineColor="transparent"
-                    theme={{  colors: { text: '#EEEEEE'} }}
-                    textColor={'#EEEEEE'}
-                    activeUnderlineColor={'#EEEEEE'}
-                    onChangeText={text => setLogin({...login, email: text})}
-                />
+                <Input underlineColor="transparent" theme={{colors: {text: '#EEEEEE'}}} textColor={'#EEEEEE'}
+                       activeUnderlineColor={'#EEEEEE'} onChangeText={text => setLogin({...login, email: text})}
+                       autoCapitalize='none'/>
                 <InputText>Password</InputText>
-                <Input
-                    underlineColor="transparent"
-                    theme={{  colors: { text: '#EEEEEE'} }}
-                    textColor={'#EEEEEE'}
-                    activeUnderlineColor={'#EEEEEE'}
-                    onChangeText={text => setLogin({...login, password: text})}
-                />
+                <Input underlineColor="transparent" theme={{colors: {text: '#EEEEEE'}}} textColor={'#EEEEEE'}
+                       activeUnderlineColor={'#EEEEEE'} onChangeText={text => setLogin({...login, password: text})}
+                       autoCapitalize='none'/>
 
-                <ActionButton mode="contained" onPress={()=> auth()}>
+                <ActionButton mode="contained" onPress={() => auth()}>
                     <ActionButtonText>Se Connecter</ActionButtonText>
                 </ActionButton>
-                <Text style={{color: '#EEEEEE', marginTop:10, fontWeight: 'bold', marginLeft: 10,
-                    marginRight: 10,}}>Mot de passe oublié ?</Text>
+                <Text style={{
+                    color: '#EEEEEE', marginTop: 10, fontWeight: 'bold', marginLeft: 10,
+                    marginRight: 10,
+                }}>Mot de passe oublié ?</Text>
             </View>
 
         </DefaultView>
--- src/screens/Profil.jsx
+++ src/screens/Profil.jsx
@@ -1,13 +1,67 @@
-import {SafeAreaView} from "react-native";
-import {Button, Text} from "react-native-paper";
+import React, {useEffect, useState} from "react";
+import {SafeAreaView, Text, FlatList, View, StyleSheet} from "react-native";
+import {Button} from "react-native-paper";
+
+import io from "socket.io-client";
+import {Input} from "../constants/style/styled";
 
 export const Profil = ({navigation}) => {
+    const [inputValue, setInputValue] = useState("");
+    const [messages, setMessages] = useState([]);
+    const socket = io("http://localhost:3000"); // Assurez-vous de spécifier le bon port ici
+
+    useEffect(() => {
+        socket.on("chat message", (message) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const handleSubmit = () => {
+        if (inputValue) {
+            const messageType = 'sent';
+            const user = "toto"// Utilisez 'sent' pour les messages envoyés par cet utilisateur
+            socket.emit("chat message", inputValue, messageType, user); // Envoyer le message avec le type
+            const message = {text: inputValue, type: messageType};
+            setMessages((prevMessages) => [...prevMessages, message]);
+            setInputValue("");
+        }
+    };
+
     return (
         <SafeAreaView>
             <Text>Profil</Text>
-            <Button mode="contained" onPress={() => navigation.navigate('Home')}>
+            <Input placeholder="Email" value={inputValue} onChangeText={(text) => setInputValue(text)}/>
+            <Button onPress={handleSubmit}>Submit</Button>
+            <Button mode="contained" onPress={() => navigation.navigate("Home")}>
                 Go to Home
             </Button>
+            <Text>Messages:</Text>
+            <FlatList data={messages} renderItem={({item}) => (
+                <View>
+                    <Text style={item.type === "sent" ? styles.sentMessage : styles.receivedMessage}>{item.text}</Text>
+                </View>
+            )} keyExtractor={(item, index) => index.toString()}/>
         </SafeAreaView>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    sentMessage: {
+        backgroundColor: "lightblue", // Style pour les messages envoyés
+        alignSelf: "flex-end",
+        padding: 5,
+        margin: 5,
+        borderRadius: 10,
+    },
+    receivedMessage: {
+        backgroundColor: "lightgreen", // Style pour les messages reçus
+        alignSelf: "flex-start",
+        padding: 5,
+        margin: 5,
+        borderRadius: 10,
+    },
+});
